2d09ba1a5447bb071a9a02d4b58c0defac25b0de
zhanglin <1003677516@qq.com>
2017-10-17 17:48:40 +0800

    张琳 2017-10-17 删除了用于测试的user活动，增加了导航栏活动和一些Xml布布局

--- app/src/main/java/com/example/foolishfan/IntelligentParking/Login.java
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/Login.java
@@ -118,7 +118,7 @@ public class Login extends Activity {                 //登录界面活动
                 }
                 editor.commit();
 
-                Intent intent = new Intent(Login.this,User.class) ;    //切换Login Activity至User Activity
+                Intent intent = new Intent(Login.this,MainActivity.class) ;    //切换Login Activity至MainActivity
                 startActivity(intent);
                 finish();
                 Toast.makeText(this, getString(R.string.login_success),Toast.LENGTH_SHORT).show();//登录成功提示
@@ -133,7 +133,7 @@ public class Login extends Activity {                 //登录界面活动
             String userPwd = mPwd.getText().toString().trim();
             int result=mUserDataManager.findUserByNameAndPwd(userName, userPwd);
             if(result==1){                                             //返回1说明用户名和密码均正确
-//                Intent intent = new Intent(Login.this,User.class) ;    //切换Login Activity至User Activity
+//                Intent intent = new Intent(Login.this,MainActivity.class) ;    //切换Login Activity至MainActivity
 //                startActivity(intent);
                 Toast.makeText(this, getString(R.string.cancel_success),Toast.LENGTH_SHORT).show();//登录成功提示
                 mPwd.setText("");
