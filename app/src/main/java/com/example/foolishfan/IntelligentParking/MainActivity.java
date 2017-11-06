d8927ab6c3b794c775557aee7f959c127f564744
RainOfPhone <792209198@qq.com>
2017-11-06 14:14:47 +0800

    添加了主页按钮的监听事件

--- app/src/main/java/com/example/foolishfan/IntelligentParking/MainActivity.java
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/MainActivity.java
@@ -10,22 +10,16 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import butterknife.ButterKnife;
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
-
-    private ImageView mImageView;
-    private NavigationView navigationView;
-    private DrawerLayout drawer;
-    private Toolbar toolbar;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     static private boolean isLogin;//全局获取当前软件的登录状态
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +30,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         SharedPreferences statusPreferences=getSharedPreferences("status",Context.MODE_PRIVATE);
         isLogin=statusPreferences.getBoolean("isLogin",false);
 
-        ButterKnife.inject(this);   //ButterKnife的绑定注解，找到参数对应的View，并自动的进行转换
-        toolbar = (Toolbar) findViewById(R.id.toolbar); //导航控件
-        setSupportActionBar(toolbar);
-
+        //获取相关按钮
+        Button addCar=(Button)findViewById(R.id.addCar);
+        Button parkNearby=(Button)findViewById(R.id.parkNearby);
+        Button wallet=(Button)findViewById(R.id.wallet);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);   //创建导航试图对象
+        ImageView mImageView = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.ivAvatar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);//导航栏
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);  //实现抽屉效果左滑拉出菜单栏
+
+        //注册监听事件
+        addCar.setOnClickListener(mainOnClick);
+        parkNearby.setOnClickListener(mainOnClick);
+        wallet.setOnClickListener(mainOnClick);
+        mImageView.setOnClickListener(mainOnClick);
+        setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);   //注册监听
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-        navigationView = (NavigationView) findViewById(R.id.nav_view);   //创建导航试图对象
         navigationView.setNavigationItemSelectedListener(this);
-
-        mImageView = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.ivAvatar);
-        mImageView.setOnClickListener(this);
-
-        //ButterKnife.bind(this);   //进阶的ButterKnife的绑定注解，目前还不会用
     }
 
     @Override
@@ -63,48 +60,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    // 动态加载右键菜单
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // 动态加载右键菜单
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    //实现切换网络连接
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will automatically handle clicks on the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        Toast toast1;
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.switchLocalhost) {
             HttpJson.setWebsite("http://10.0.2.2/ParkingWeb/");
-            toast1 = Toast.makeText(getApplicationContext(), "已设置为10.0.2.2", Toast.LENGTH_SHORT);
-            toast1.setGravity(Gravity.BOTTOM, 0, 0);
-            toast1.show();
+            Toast.makeText(getApplicationContext(), "已设置为10.0.2.2", Toast.LENGTH_SHORT).show();
         }
         if (id == R.id.switchServer) {
             HttpJson.setWebsite("http://120.78.173.73/ParkingWeb/");
-            toast1 = Toast.makeText(getApplicationContext(), "已设置为120.78.173.73", Toast.LENGTH_SHORT);
-            toast1.setGravity(Gravity.BOTTOM, 0, 0);
-            toast1.show();
+            Toast.makeText(getApplicationContext(), "已设置为120.78.173.73", Toast.LENGTH_SHORT).show();
         }
         if (id == R.id.switchLAN) {
             HttpJson.setWebsite("http://192.168.155.1/ParkingWeb/");
-            toast1 = Toast.makeText(getApplicationContext(), "已设置为192.168.155.1", Toast.LENGTH_SHORT);
-            toast1.setGravity(Gravity.BOTTOM, 0, 0);
-            toast1.show();
+            Toast.makeText(getApplicationContext(), "已设置为192.168.155.1", Toast.LENGTH_SHORT).show();
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     //设置点击左边菜单栏每一个选项的回应方式
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
-        String string = null;
         switch (id){
             case R.id.nav_me:
                 break;
@@ -130,18 +117,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.ivAvatar){
-            if(isLogin){
-                Intent intent=new Intent(this,User.class);
-                startActivity(intent);
-            }else{
-                Intent intent = new Intent(this,Login.class);   //若点击用户登录则切换MainActivity至Login Activity
-                startActivity(intent);
+    //页面点击事件
+    View.OnClickListener mainOnClick= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id=v.getId();
+            switch (id){
+                case R.id.ivAvatar://点击头像的跳转事件
+                    if(isLogin){
+                        Intent intent=new Intent(getApplicationContext(),User.class);
+                        startActivity(intent);
+                    }else{
+                        Intent intent = new Intent(getApplicationContext(),Login.class);
+                        startActivity(intent);
+                    }
+                    break;
+                case R.id.addCar://点击添加车辆的监听事件
+                    break;
+                case R.id.parkNearby://点击附近停车场的监听事件
+                    break;
+                case R.id.wallet://点击我的钱包的监听事件
+                    break;
             }
         }
-    }
-
+    };
 }
 
