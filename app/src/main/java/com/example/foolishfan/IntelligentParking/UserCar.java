5d3267a4114a78d7ae80f24590b3bd8730591d62
Charlie1849764736 <1849764736@qq.com>
2017-12-04 16:40:06 +0800

    Signed-off-by: Charlie1849764736 <1849764736@qq.com>

--- app/src/main/java/com/example/foolishfan/IntelligentParking/UserCar.java
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/UserCar.java
@@ -12,103 +12,108 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.example.foolishfan.IntelligentParking.Util.CarRecord;
 import com.example.foolishfan.IntelligentParking.Util.HttpJson;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class UserCar extends AppCompatActivity {          //用户车辆信息界面
-    TextView plateNumber;
-    TextView remarks;
+import java.util.ArrayList;
+import java.util.List;
 
-    protected Handler handler = new Handler() {
+public class UserCar extends AppCompatActivity {          //用户车辆信息界面
+    private ListView listView;
+    private List<CarRecord> datas = new ArrayList<CarRecord>();//要填充的数据
+
+    //主线程创建消息处理器
+    private Handler handler = new Handler(){
         public void handleMessage(Message msg) {
-            if (msg.obj != null) {//如果不为空
-                //解析json数据并保存
-                String jsonStr = msg.obj.toString();
-                //解析json数据，按键名存在SharedPreference中
-                JSONObject jsonObject;
-                String plateNumberString=null;
+            if (msg.obj != null) {
                 try {
-                    jsonObject = new JSONObject(jsonStr);
-                    plateNumberString=jsonObject.getString("plateNumber");
+                    //把传回来的字符串转换成json数组
+                    JSONArray jsonArray = new JSONArray(msg.obj.toString());
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);//解析为json对象
+                        CarRecord carrecord = new CarRecord();
+                        carrecord.setImageId(jsonObject.getInt("id"));
+                        carrecord.setPlateNumber(jsonObject.getString("plateNumber"));
+                        carrecord.setRemarks(jsonObject.getString("remarks"));//传入CarRecord类
+                        datas.add(carrecord);//添加到要填充的数据列表
+                    }
+                    listView.setAdapter(new MyAdapter());//传入适配器对象，和listview建立关联
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                //保存车牌号码到手机
-                SharedPreferences.Editor recordEditor = getSharedPreferences("user", Context.MODE_PRIVATE).edit();
-                recordEditor.putString("plateNumber", plateNumberString);
-                //将相应消息放在对应的控件显示
-                SharedPreferences userPref=getSharedPreferences("user",Context.MODE_PRIVATE);
-                String remarksString=userPref.getString("remarks",null);
-                remarks.setText(remarksString);
-                plateNumber.setText(plateNumberString);
-            } else {
-                Toast.makeText(getApplicationContext(), "网络错误", Toast.LENGTH_SHORT).show();
             }
-            super.handleMessage(msg);
-        }
+        };
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_car);
+        listView = (ListView) findViewById(R.id.list_view);
+        select();//获取数据
 
-       @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_user_car);
+        //设置toolbar导航栏，设置导航按钮
+        Toolbar usercar_toolbar = (Toolbar) findViewById(R.id.usercar_toolbar);
+        setSupportActionBar(usercar_toolbar);
+        usercar_toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 
-
-            //设置toolbar导航栏，设置导航按钮
-            Toolbar usercar_toolbar = (Toolbar) findViewById(R.id.usercar_toolbar);
-            setSupportActionBar(usercar_toolbar);
-            usercar_toolbar.setNavigationOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    finish();
-                }
-            });
-
-           plateNumber = (TextView) findViewById(R.id.plateNumber);//显示用户车牌号码
-           remarks = (TextView) findViewById(R.id.remarks);//显示用户车辆备注
-
-           getUserCarInfo();
-        }
-
-    public void getUserCarInfo() {
+    private void select(){
         //1.从sharedPreference里面获取当前账户手机号
         SharedPreferences pref = getSharedPreferences("user", Context.MODE_PRIVATE);
         String mobile = pref.getString("mobile", null);
 
         //2.将用户手机号转为json
-        JSONObject json = new JSONObject();
+        JSONObject json=new JSONObject();
         try {
-            json.put("mobile", mobile);
+            json.put("mobile",mobile);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
         //3.把手机号发送到服务器上进行查询
-        String path = "user/carinfo_inquiry.php";
-        HttpJson http = new HttpJson(path, json.toString(), handler);
+        String path="user/carinfo_inquiry.php";
+        HttpJson http=new HttpJson(path,json.toString(),handler);
         new Thread(http.getHttpThread()).start();
     }
-    //解析Json数据
-    private void parseJSONWithJSONObject(String jsonData) {
-        try {
 
-            //？得到json数组
-            JSONObject jsonObject = new JSONObject(jsonData);
-            String plateNumber=jsonObject.getString("plateNumber");
-
-            //保存当前用户记录
-            SharedPreferences.Editor recordEditor = getSharedPreferences("user", Context.MODE_PRIVATE).edit();
-            recordEditor.putString("plateNumber", plateNumber);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+    class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {return datas.size();}
+        @Override
+        public Object getItem(int position) {return datas.get(position);}
+        @Override
+        public long getItemId(int position) {return position;}
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            //为子项动态加载布局：若有缓存的加载好的布局则使用；否则重新加载
+            if (convertView == null){
+                view = View.inflate(UserCar.this, R.layout.activity_user_car_item, null);
+                TextView imageId = (TextView) view.findViewById(R.id.tv_id);
+                TextView plateNumber = (TextView) view.findViewById(R.id.tv_plateNumber);
+                TextView remarks = (TextView) view.findViewById(R.id.tv_remarks) ;
+                imageId.setText(String.valueOf(datas.get(position).getImageId()));
+                plateNumber.setText(datas.get(position).getPlateNumber());
+                remarks.setText(datas.get(position).getRemarks());
+            }else {
+                view = convertView;
+            }
+            return view;
         }
     }
-
 }
---
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/Util/CarRecord.java
@@ -0,0 +1,30 @@
+package com.example.foolishfan.IntelligentParking.Util;
+
+/**
+ * Created by liangjiacheng on 2017/12/2 0002.
+ */
+
+public class CarRecord {
+    private int imageId;
+    private String plateNumber;
+    private String remarks;
+
+    public int getImageId() {
+        return imageId;
+    }
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+    public String getRemarks() {
+        return remarks;
+    }
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+}
