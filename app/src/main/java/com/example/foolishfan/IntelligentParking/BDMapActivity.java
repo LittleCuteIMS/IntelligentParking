1a7bb697ae6492e7835f87a7516da0fa2adc2300
zhanglin <1003677516@qq.com>
2017-12-05 20:48:21 +0800

    增加了地图覆盖物以及覆盖物点击事件
    Signed-off-by: zhanglin <1003677516@qq.com>

--- app/src/main/java/com/example/foolishfan/IntelligentParking/BDMapActivity.java
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/BDMapActivity.java
@@ -2,6 +2,7 @@ package com.example.foolishfan.IntelligentParking;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -10,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
@@ -18,11 +21,17 @@ import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.InfoWindow;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
 
 import java.util.ArrayList;
@@ -32,7 +41,7 @@ public class BDMapActivity extends AppCompatActivity {
     public LocationClient mLocationClient;//定位类
     private MapView mapView;//地图对象
     private BaiduMap baiduMap;//百度地图对象
-    private boolean isFirstLocate = true;
+    private boolean isFirstLocate = true;//如果是第一次定位的话要将自己的位置显示在地图中间
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,17 +86,25 @@ public class BDMapActivity extends AppCompatActivity {
         }
     }
 
+    public class MyLocationListener implements BDLocationListener{
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation || bdLocation.getLocType() == BDLocation.TypeNetWorkLocation ){
+                navigateTo(bdLocation);
+            }
+        }
+    }
+
     private void navigateTo(BDLocation location){
         if (isFirstLocate){
             LatLng ll = new LatLng(location.getLatitude(),location.getLongitude());//LatLng是用于存放经纬度值的类，接收两个参数：维度，经度
 
             //创建一个新的MapStatus
-            MapStatus mapStatus = new MapStatus.Builder()
-                    //定位到定位点
-                    .target(ll)
-                    //决定缩放的尺寸
-                    .zoom(16)
-                    .build();
+            MapStatus mapStatus = new MapStatus.Builder().target(ll).zoom(16).build();
+            mapView.removeViewAt(1);// 不显示百度地图Logo
+            mapView.showZoomControls(false);// 不显示缩放比例尺
+
             //利用MapStatus构建一个MapStatusUpdate对象
             MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mapStatus);
             //更新BaiduMap，此时BaiduMap的界面就会从初始位置（北京），移动到定位点
@@ -112,6 +129,61 @@ public class BDMapActivity extends AppCompatActivity {
         option.setScanSpan(10000);//设置更新的时间间隔
         option.setIsNeedAddress(true);
         mLocationClient.setLocOption(option);
+        showMarkers();//显示停车场位置图标
+    }
+
+    private void showMarkers(){
+        BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.icon_marka);//构建Marker图标
+        final LatLng point1 = new LatLng(30.638251183288837,104.08398602054602);//设置停车场的经纬度
+        OverlayOptions option = new MarkerOptions().position(point1).icon(bitmap); //构建MarkerOption，用于在地图上添加Marker
+        baiduMap.addOverlay(option);//在地图上添加Marker，并显示
+        //设置点击事件
+        baiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener()  {
+            @Override
+            public boolean onMarkerClick(final Marker marker)  {
+                TextView textView = new TextView(getApplication());//创建InfoWindow展示的view
+                textView.setBackgroundResource(R.color.colorHalftransparent);//设置InfoWindow的背景半透明
+                textView.setText("四川大学停车场 ");
+                InfoWindow mInfoWindow = new InfoWindow(textView, point1, 80);//创建InfoWindow , 传入view，地理坐标，y轴偏移量
+                baiduMap.showInfoWindow(mInfoWindow);//显示InfoWindow
+
+                ImageView imageView = (ImageView) findViewById(R.id.photo);
+                imageView.setImageResource(R.drawable.advanse1);
+
+                TextView textView1 = (TextView) findViewById(R.id.tohere);
+                textView1.setBackgroundColor(Color.WHITE);
+                textView1.setText("到这里去");
+
+                TextView textView2 = (TextView) findViewById(R.id.parking_name);
+                textView2.setBackgroundColor(Color.WHITE);
+                textView2.setText("名称：");
+                textView2.append(textView.getText());
+
+                TextView textView3 = (TextView) findViewById(R.id.parking_addr);
+                textView3.setBackgroundColor(Color.WHITE);
+                textView3.setText("地址：成都市武侯区一环路南一段");
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        baiduMap.setMyLocationEnabled(true); //开启定位
+        if(!mLocationClient.isStarted()){
+            mLocationClient.start();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //关闭定位
+        baiduMap.setMyLocationEnabled(false);
+        if(mLocationClient.isStarted()){
+            mLocationClient.stop();
+        }
     }
 
     @Override
@@ -154,16 +226,6 @@ public class BDMapActivity extends AppCompatActivity {
                 }
                 break;
             default:
-        }
-    }
-
-    public class MyLocationListener implements BDLocationListener {
-
-        @Override
-        public void onReceiveLocation(BDLocation bdLocation) {
-            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation || bdLocation.getLocType() == BDLocation.TypeNetWorkLocation ){
-                navigateTo(bdLocation);
-            }
         }
     }
 
