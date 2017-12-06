78cfff0deda6ab3aa917efcb6dbb6c5e70bb7772
nicokani <2917820775@qq.com>
2017-12-06 10:25:03 +0800

    停车历史查询

--- app/src/main/java/com/example/foolishfan/IntelligentParking/ParkingHistory.java
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/ParkingHistory.java
@@ -1,52 +1,142 @@
 package com.example.foolishfan.IntelligentParking;
 
-/**
- * Created by LiangJiacheng on 2017/11/19 0019.
- */
-
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
+import android.view.LayoutInflater;
+import android.view.View;
+
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.foolishfan.IntelligentParking.Util.HttpJson;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.util.ArrayList;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
-public class ParkingHistory extends AppCompatActivity {          //用户停车记录信息界面
+/*
+Created by RongYuting on 2017/12/3.
+ */
+class ParkingHistory extends ApplicationManagement{
 
-    private EditText mPlateNumber;                            //用户车牌号
-    private EditText mRemark;                                 //备注编辑
-    private EditText mParkingLotName;                        //停车场名称
-    private EditText mParkingLotLocation;                    //停车场地址
-    private EditText mEnterTime;                            //进入停车场时间
-    private EditText mLeaveTime;                            //离开停车场时间
-    private EditText mParkingTime;                          //停车时长
-    private EditText mParkingCost;                         //用户停车扣费
-    private Handler handler;                   //登录接收服务器返回的信息
+    private ListView lv;
+    List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();//设置数据源或者说数据集合
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parkinghistory);
+        lv = (ListView) findViewById(R.id.listview);
+
+        getData();
 
         //设置toolbar导航栏，设置导航按钮
-        Toolbar parkinghistory_toolbar = (Toolbar) findViewById(R.id.parkinghistory_toolbar);
-        setSupportActionBar(parkinghistory_toolbar);
-        parkinghistory_toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+        Toolbar parking_toolbar = (Toolbar) findViewById(R.id.parking_toolbar );
+        setSupportActionBar(parking_toolbar);
+        parking_toolbar.setNavigationOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
     }
+
+    //从sharedPreference中获取数据并发送到服务器进行查询
+    private void getData(){
+        //1.从sharedPreference里面获取当前账户手机号
+        SharedPreferences pref = getSharedPreferences("user", Context.MODE_PRIVATE);
+        String mobile = pref.getString("mobile", null);
+
+        //2.将用户手机号转为json
+        JSONObject json=new JSONObject();
+        try {
+            json.put("mobile",mobile);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //3.把手机号发送到服务器上进行查询
+        String path="parkPHP/parking_record_select.php";
+        HttpJson http=new HttpJson(path,json.toString(),handler);
+        new Thread(http.getHttpThread()).start();
+    }
+
+    //将返回的json数据进行转化
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg) {
+            if (msg.obj != null) {
+                try {
+                    //把传回来的字符串转换成json数组
+                    JSONArray jsonArray = new JSONArray(msg.obj.toString());
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);//解析为json对象
+                        Map<String,Object> map = new HashMap<String,Object>();
+                        map.put("plate_number",jsonObject.getString("plate_number"));
+                        map.put("park_id",jsonObject.getString("park_id"));
+                        map.put("in_datetime",jsonObject.getString("in_datetime"));
+                        map.put("out_datetime",jsonObject.getString("out_datetime"));
+                        list.add(map);
+                    }
+                    lv.setAdapter(new RecordAdapter());//传入适配器对象，和listview建立关联
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    };
+
+    //自定义适配器RecordAdapter,继承BaseAdapter
+    class RecordAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {                 //必填，是渲染的行数
+            return list.size();
+        }
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+
+            if (convertView == null){
+                // view = View.inflate(ParkingHistory.this, R.layout.parkinghistory_item, null);
+            view = LayoutInflater.from(ParkingHistory.this).inflate(R.layout.parkinghistory_item, parent,false);
+
+                   TextView plateNumber = (TextView) view.findViewById(R.id.lv_plate);
+                    TextView parkId= (TextView) view.findViewById(R.id.lv_parkId);
+                    TextView inTime = (TextView) view.findViewById(R.id.lv_intime);
+                    TextView outTime = (TextView) view.findViewById(R.id.lv_outtime);
+                    Map maplist = list.get(position);
+                    plateNumber.setText((String)maplist.get("plate_number"));
+                    parkId.setText((String)maplist.get("park_id"));
+                    inTime.setText((String)maplist.get("in_datetime"));
+                    outTime.setText((String)maplist.get("out_datetime"));
+
+           }else {
+                view = convertView;
+            }
+            return view;
+        }
+    }
 }
+
+
