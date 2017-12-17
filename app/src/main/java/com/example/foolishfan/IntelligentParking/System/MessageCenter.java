b7d62ceef6f12ee66bf2b533feda6eeb17359cac
nicokani <2917820775@qq.com>
2017-12-17 20:29:16 +0800

    消息通知

--- app/src/main/java/com/example/foolishfan/IntelligentParking/System/MessageCenter.java
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/System/MessageCenter.java
@@ -4,35 +4,50 @@ package com.example.foolishfan.IntelligentParking.System;
  * Created by LiangJiacheng on 2017/11/19 0019.
  */
 
+
+
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
+import android.widget.TextView;
 import com.example.foolishfan.IntelligentParking.R;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import cn.jpush.android.api.JPushInterface;
 
-public class MessageCenter extends AppCompatActivity {          //消息中心界面
-
-    private EditText mMessage;                        //用户车牌号
-    private Handler handler;                   //登录接收服务器返回的信息
-
+public class MessageCenter extends AppCompatActivity {
+    List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_center);
+        TextView tv = (TextView)findViewById(R.id.textview);
+        String content = getIntent().getStringExtra("content");
+        if(content!= null){
+            //将传回来的信息放在SharedPreferences
+            SharedPreferences.Editor recordEditor = getSharedPreferences("message", Context.MODE_PRIVATE).edit();
+            recordEditor.putString("message", content);
+            recordEditor.apply();
+        }else {
+            tv.setText("");
+        }
+        //取出数据
+        SharedPreferences pref = getSharedPreferences("user", Context.MODE_PRIVATE);
+        String Jmessage = pref.getString("message", null);
+        tv.setText(Jmessage);
+
+        //设置为调试模式，具体发布的时候可以直接设置为false
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
 
         //设置toolbar导航栏，设置导航按钮center
         Toolbar messagecenter_toolbar = (Toolbar) findViewById(R.id.messagecenter_toolbar);
         setSupportActionBar(messagecenter_toolbar);
-        messagecenter_toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+        messagecenter_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
