c8955a41df5ccd119ccec68e4dbc465f799338a6
RainOfPhone <792209198@qq.com>
2017-12-21 10:04:21 +0800

    计时计费页面布局

--- app/src/main/java/com/example/foolishfan/IntelligentParking/Util/QRcode.java
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/Util/QRcode.java
@@ -2,12 +2,10 @@ package com.example.foolishfan.IntelligentParking.Util;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.example.foolishfan.IntelligentParking.ParkNavigation.ParkInfoActivity;
+import com.example.foolishfan.IntelligentParking.SystemFunction.HourlyBilling;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -72,7 +70,7 @@ public class QRcode {
         try {
             JSONObject jsonObj=new JSONObject(jsonRs);
             if(jsonObj.getString("mode").equals("park")){
-                Intent intent_Login_to_Register = new Intent(context, ParkInfoActivity.class);
+                Intent intent_Login_to_Register = new Intent(context, HourlyBilling.class);
                 Bundle bundle=new Bundle();//创建email内容
                 bundle.putString("parkInfoJson",jsonRs);
                 intent_Login_to_Register.putExtra("qr_code_info",bundle);
