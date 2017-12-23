22b94247c9604e9b74f49fbbf957f4cecba435a2
RainOfPhone <792209198@qq.com>
2017-12-23 23:31:21 +0800

    添加HttpJsonModified.java，一个activity中使用一个handler 添加了spinner

--- app/src/main/java/com/example/foolishfan/IntelligentParking/SystemFunction/HourlyBilling.java
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/SystemFunction/HourlyBilling.java
@@ -1,17 +1,32 @@
 package com.example.foolishfan.IntelligentParking.SystemFunction;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.foolishfan.IntelligentParking.R;
+import com.example.foolishfan.IntelligentParking.Util.HttpJson;
+import com.example.foolishfan.IntelligentParking.Util.HttpJsonModified;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 蔡创 on 2017-12-13.
@@ -19,6 +34,21 @@ import org.json.JSONObject;
  */
 
 public class HourlyBilling extends AppCompatActivity {
+
+    final private int CARINFO=1;
+    private Handler billingHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch(msg.what){
+                case CARINFO:
+                    if(msg.obj != null){
+                        setCarSpinner(msg.obj.toString());
+                    }
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +67,28 @@ public class HourlyBilling extends AppCompatActivity {
 
         JSONObject parkInfoJsonObj = null;
         String park_id = null;
-        try {
-            parkInfoJsonObj = getParkInfoJson();
-            park_id = showParkInfo(parkInfoJsonObj);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        parkInfoJsonObj = getParkInfoJson();
+        park_id = showParkInfo(parkInfoJsonObj);
+        getCarInfo();
         setStartPark();
     }
 
     //从二维码中获取停车场信息
-    private JSONObject getParkInfoJson() throws JSONException {
+    private JSONObject getParkInfoJson(){
         //接受二维码扫描的信息
         Intent intent = getIntent();// 收取 email
         Bundle bundle = intent.getBundleExtra("qr_code_info");// 打开 email
         String parkInfoStr = bundle.getString("parkInfoJson");//读取内容能够
-        return new JSONObject(parkInfoStr);//转换为json对象
+        try {
+            return new JSONObject(parkInfoStr);//转换为json对象
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     //显示停车场的相关信息，返回停车场的编号
-    private String showParkInfo(JSONObject parkInfoJsonObj) throws JSONException {
+    private String showParkInfo(JSONObject parkInfoJsonObj){
         //获取显示停车场信息的textview
         TextView parkNameView, parkPhoneView, parkChargeView;
         parkNameView = (TextView) findViewById(R.id.billing_name);
@@ -65,15 +97,53 @@ public class HourlyBilling extends AppCompatActivity {
         String park_id = null;
 
         //将二维码扫描到的信息放入相应的textview
-        String parkName = parkNameView.getText().toString() + parkInfoJsonObj.getString("park_name");
-        parkNameView.setText(parkName);
-        String parkPhone = parkPhoneView.getText().toString() + parkInfoJsonObj.getString("park_phone");
-        parkPhoneView.setText(parkPhone);
-        String parkCharge = parkChargeView.getText().toString() + parkInfoJsonObj.getString("park_charge");
-        parkChargeView.setText(parkCharge);
-        park_id = parkInfoJsonObj.getString("park_id");
-
+        String parkName = null;
+        try {
+            parkName = parkNameView.getText().toString() + parkInfoJsonObj.getString("park_name");
+            parkNameView.setText(parkName);
+            String parkPhone = parkPhoneView.getText().toString() + parkInfoJsonObj.getString("park_phone");
+            parkPhoneView.setText(parkPhone);
+            String parkCharge = parkChargeView.getText().toString() + parkInfoJsonObj.getString("park_charge");
+            parkChargeView.setText(parkCharge);
+            park_id = parkInfoJsonObj.getString("park_id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return park_id;
+    }
+
+    //从服务器获取用户车辆信息
+    private void getCarInfo(){
+        SharedPreferences pref = getSharedPreferences("user", Context.MODE_PRIVATE);
+        String mobile = pref.getString("mobile", null);
+        JSONObject json = new JSONObject();
+        try {
+            json.put("mobile", mobile);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String path="user/carinfo_inquiry.php";
+        HttpJsonModified http=new HttpJsonModified(path,json.toString(),billingHandler,CARINFO);
+        new Thread(http.getHttpThread()).start();
+    }
+
+    //对车辆选择下拉列表设置下拉项
+    private void setCarSpinner(String carInfoStr){
+        JSONArray jsonArray = null;
+        JSONObject jsonTmp = null;
+        List<String> carList=new ArrayList<String>();
+        try {
+            jsonArray = new JSONArray(carInfoStr);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                jsonTmp = jsonArray.getJSONObject(i);
+                carList.add(jsonTmp.getString("plate_number"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Spinner carSpinner=(Spinner)findViewById(R.id.car_spinner);
+        ArrayAdapter<String> carInfoAdapter=new ArrayAdapter<String>(HourlyBilling.this,android.R.layout.simple_spinner_item, carList);
+        carSpinner.setAdapter(carInfoAdapter);
     }
 
     //对开始停车按钮设置监听事件
@@ -87,4 +157,5 @@ public class HourlyBilling extends AppCompatActivity {
             }
         });
     }
+
 }
---
+++ app/src/main/java/com/example/foolishfan/IntelligentParking/Util/HttpJsonModified.java
@@ -0,0 +1,108 @@
+package com.example.foolishfan.IntelligentParking.Util;
+
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Administrator on 2017/10/8 0008.
+ * 此类为客户端向服务器发送json格式的数据并获取服务器的返回信息
+ */
+
+public class HttpJsonModified {
+    static private  String website="http://120.78.173.73/ParkingWeb/";    //设置访问IP地址值
+    private String path=null;     //获取访问的php文件的URL地址
+    private String json=null;       //获取要传输的json格式字符串数据
+    private Handler handler=null;   //接受子线程发送的数据， 并用此数据配合主线程更新UI
+    private HttpThread httpThread;  //处理通信的线程
+    private int messageNum;
+
+    public HttpJsonModified(String initPath, String initJson, Handler initHandler){//不带消息序列的构造函数
+
+        path=initPath;
+        json=initJson;
+        handler=initHandler;    //获取主线程的handler
+        httpThread=new HttpThread();
+    }
+
+    public HttpJsonModified(String initPath, String initJson, Handler initHandler, int initMessageNum){//带消息序列的构造函数
+
+        path=initPath;
+        json=initJson;
+        handler=initHandler;    //获取主线程的handler
+        httpThread=new HttpThread();
+        messageNum=initMessageNum;
+    }
+
+    public HttpThread getHttpThread(){//获取网络通信线程
+        return httpThread;
+    }
+
+    static public void setWebsite(String initWebsite){
+        website=initWebsite;
+    }
+
+    private class HttpThread implements Runnable{//处理通信的线程的类
+        public void run() {
+            URL url=null;
+            String result="";   //获取服务器返回的内容
+            try {
+                url = new URL(website+path);//构造URL对象
+                //打开连接
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");//设置请求为POST方式
+                conn.setDoInput(true);//设置输入
+                conn.setDoOutput(true);//设置输出
+                conn.setUseCaches(false);//禁用缓存
+
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("Charset", "UTF-8");
+                //设置文件类型
+                conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                //设置接收类型
+                conn.setRequestProperty("accept","application/json");
+                //往服务器里面发送数据
+                if(json!=null&&!TextUtils.isEmpty(json)){
+                    //设置文件长度
+                    conn.setRequestProperty("Content-Length",String.valueOf(json.getBytes().length));
+                    //打开链接
+
+                    DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+                    //组织要发送的数据
+                    out.write(json.getBytes());//将数据写入POST数据流
+                    out.flush();//刷新流
+                    out.close();//关闭流
+                }
+                //判断请求是否成功
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK)
+                {
+                    InputStreamReader in=null;
+                    //获取服务器的响应内容
+                    in = new InputStreamReader(conn.getInputStream());
+                    BufferedReader buffer=new BufferedReader(in);
+                    String inputLine=null;
+                    while((inputLine=buffer.readLine())!=null) {//循环读取内容
+                        result += inputLine;
+                    }
+                    in.close();//关闭读取
+                    conn.disconnect();//断开连接
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            Message message = new Message();//创建一个Message消息
+            if(result!=""){
+                message.what=messageNum;
+                message.obj=result;//为消息添加从服务器上获取的消息
+            }
+            handler.sendMessage(message);//发送消息
+        }
+    }
+}
